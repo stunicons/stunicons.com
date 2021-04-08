d241f65183d43f0cdef99aacd45d6c3ad418a60c
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-08 17:35:47 +0200

    making version component

---
+++ components/Version.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="version">
+    <h6>{{ version }}</h6>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+const { version } = require('@/package.json')
+
+@Component
+export default class Navbar extends Vue {
+  version = version
+}
+
+</script>
+
+<style lang="scss" scoped>
+.version{
+  background:$clr-primary-lighten;
+  color:$clr-primary;
+}
+</style>
