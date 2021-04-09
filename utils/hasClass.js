82b754df541ba1582e5267450007a550bf43845a
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-09 16:26:35 +0200

    fix: hasClass method

--- utils/hasClass.js
+++ utils/hasClass.js
@@ -2,5 +2,5 @@ export default function (el,className){
   if(className.startsWith('.'))
     className = className.split('').splice(1,className.length).join('')
 
-  return el.classList.find(cls => cls === className).length > 0
+  return !![...el.classList].find(cls => cls === className)
 }
