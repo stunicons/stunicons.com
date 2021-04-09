e5b76bb1fb5214bca0b354d9e1fd20e787eb4fa5
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-09 17:55:29 +0200

    feat: finalising

---
+++ components/Search/Search.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="search">
+  <div class="search--wrapper">
+    <div class="search--filter">
+      <search-filter />
+    </div>
+    <div class="search--input">
+      <SearchInput />
+    </div>
+    <div class="search--collection">
+      <collection-accessor/>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import SearchInput from "./SearchInput";
+import CollectionAccessor from "./Collection-accessor";
+import SearchFilter from "./SearchFilter";
+export default {
+name: "Search",
+  components: {SearchFilter, CollectionAccessor, SearchInput}
+}
+</script>
+
+<style lang="scss" scoped>
+.search{
+  @apply my-24;
+  &--wrapper{
+    @apply flex ;
+
+  }
+
+  &--input{
+    @apply mx-2;
+    flex:1;
+  }
+}
+</style>
