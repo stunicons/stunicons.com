65847ed2285653e6084d51268b89b12e29af7d9b
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-09 16:09:05 +0200

    feat: hasClass method

---
+++ utils/hasClass.js
@@ -0,0 +1,6 @@
+export default function (el,className){
+  if(className.startsWith('.'))
+    className = className.split('').splice(1,className.length).join('')
+
+  return el.classList.find(cls => cls === className).length > 0
+}
