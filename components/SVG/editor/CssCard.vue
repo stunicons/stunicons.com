fcfbdd6d081db4fa7251490b6ccdfc046f992fd8
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-11 23:48:45 +0200

    feat: create css card

---
+++ components/SVG/editor/CssCard.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="css-card">
+  <div class="css-card--wrapper">
+    <div class="tag">
+      <pre><i :class="iconClass"></i></pre>
+    </div>
+    <div class="copy-icon"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "CssCard",
+  props:{
+    iconClass:{type:String}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
