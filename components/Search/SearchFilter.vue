5958c7670fd0b5c95b7237b7734f8d7d7cb518c8
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-11 10:53:31 +0200

    perf: indentation

--- components/Search/SearchFilter.vue
+++ components/Search/SearchFilter.vue
@@ -5,11 +5,11 @@
       <span ref="icon" class="icon "> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 11.828l-2.828 2.829-1.415-1.414L12 9l4.243 4.243-1.415 1.414L12 11.828z"/></svg> </span>
     </div>
     <transition name="slide-down-fade">
-       <div class="filter--list-to-select" v-if="listActive">
-      <ul>
-        <li @click="selectCategory(i)" v-for="(f,i) in selectionList" :key="f"> {{f}} </li>
-      </ul>
-    </div>
+     <div class="filter--list-to-select" v-if="listActive">
+        <ul>
+          <li @click="selectCategory(i)" v-for="(f,i) in selectionList" :key="f"> {{f}} </li>
+        </ul>
+      </div>
     </transition>
   </div>
 </template>
