0272e972e7e255e4a874ca576ab30c013892385f
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-11 23:28:33 +0200

    refactor: reading icons from storage

---
+++ components/SVG/reusable/copy-card.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="copy-card">
+ <div class="copy-card--wrapper">
+    <div class="copy-card--icon">
+    <slot name="icon"> </slot>
+  </div>
+  <div class="copy-card--name">
+    <slot name="name"></slot>
+  </div>
+ </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "copy-card"
+}
+</script>
+
+<style lang="scss" scoped>
+.copy-card{
+  width:8rem;
+  background-color:$clr-primary-lighten;
+
+  &--wrapper{
+    @apply py-4;
+  }
+
+  &--icon{
+      i,svg{
+        color:$clr-primary;
+        fill:$clr-primary;
+      }
+  }
+  &--name{
+    color:$clr-primary;
+  }
+
+}
+</style>
