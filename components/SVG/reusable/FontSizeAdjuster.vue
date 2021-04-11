3a3d9aef605350183c6087a63fd45e2cea720a3b
Ntwari Clarance Liberiste <ntwaricliberi@gmail.com>
2021-04-11 23:33:33 +0200

    feat: create font-size adjustor

---
+++ components/SVG/reusable/FontSizeAdjuster.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="fontsize-adjuster">
+  <div class="fontsize-adjuster--wrapper">
+    <p>size:</p>
+    <div class="adjuster">
+      <div class="line"></div>
+      <div class="ball"></div>
+    </div>
+    <p>{{currentFont}}px</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "FontSizeAdjuster",
+  data() {
+    return {
+      currentFont:"24px",
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
