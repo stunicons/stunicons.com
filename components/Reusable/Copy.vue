78936514e996e5faccb9036d5185254f2ad3252c
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-19 18:57:35 +0200

    style: styling component

--- components/Reusable/Copy.vue
+++ components/Reusable/Copy.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="copy">
-  <div class="copy--wrapper"></div>
+  <div class="copy--wrapper">
+    <div class="copy--icon">
+      <slot name="icon"></slot>
+    </div>
+    <h4 class="copy--title">
+      <slot name="title"></slot>
+    </h4>
+  </div>
 </div>
 </template>
 
@@ -10,6 +17,26 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.copy{
+  height: fit-content;
+  width: fit-content;
 
+  &--wrapper{
+   @apply px-10 py-5 rounded-md;
+    background-color: $clr-primary-lighten;
+
+  }
+
+  &--icon{
+   @apply px-2;
+    svg{
+      fill:$clr-primary;
+    }
+  }
+  &--title{
+   @apply px-2;
+    color:$clr-primary;
+  }
+}
 </style>
