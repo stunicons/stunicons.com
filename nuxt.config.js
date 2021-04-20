d19649676afe5211a1b055b9fd776443ce7d1679
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-20 09:43:46 +0200

    build: new font

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,9 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap' }
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code&display=swap' }
       ]
   },
 
