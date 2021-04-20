0279e6f79a1bcd64cd5b82b123a4ec3b9259dcdb
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-20 07:36:30 +0200

    feat: dynamic data

--- components/IconEditor/CodeHighlight.vue
+++ components/IconEditor/CodeHighlight.vue
@@ -1,14 +1,22 @@
 <template>
 <div class="code-highlight">
   <div class="code-highlight--wrapper">
-
+    {{formattedCode}}
   </div>
 </div>
 </template>
 
 <script>
 export default {
-name: "CodeHighlight",
+  name: "CodeHighlight",
+  props:{
+    code:{required:true,type:String}
+  },
+  computed:{
+    formattedCode(){
+      return this.code
+    }
+  }
 
 }
 </script>
