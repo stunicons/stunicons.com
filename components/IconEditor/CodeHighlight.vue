2344ccf0596b708c2b3b364fc2c97e23e937cbbe
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-20 07:35:24 +0200

    feat: code highlighter component

---
+++ components/IconEditor/CodeHighlight.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="code-highlight">
+  <div class="code-highlight--wrapper">
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "CodeHighlight",
+
+}
+</script>
+
+<style scoped>
+
+</style>
