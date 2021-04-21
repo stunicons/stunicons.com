532d2b496f0eda1bb45c154fc6d0deb8cd366779
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-21 09:39:54 +0200

    style: styling color pickers

--- components/SVG/reusable/ColorPicker.vue
+++ components/SVG/reusable/ColorPicker.vue
@@ -1,27 +1,55 @@
 <template>
 <div class="color-picker">
   <div class="color-picker--wrapper">
-    <p>color:</p>
-    <div class="colors">
-      <div class="color black"></div>
-      <div class="color orange"></div>
-      <div class="color blue"></div>
-      <div class="color red"></div>
-      <div class="color green"></div>
-    </div>
-    <div class="add-color">
-      <div class="icon"></div>
-    </div>
+    <p>Color:</p>
+    <ul>
+      <li class="black active"></li>
+      <li class="orange"></li>
+      <li class="blue"></li>
+      <li class="red"></li>
+      <li class="green"></li>
+    </ul>
   </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: "ColorPicker"
+name: "ColorPicker"
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.color-picker{
+  &--wrapper{
+    ul{
+      @apply flex;
+
+      li{
+        @apply mx-2;
+        box-sizing: content-box;
+        width:18px;
+        height: 18px;
+        border-radius: 50%;
+
+        &.active{
+          border:3px solid $bg;
+          box-shadow: 0 4px 4px 0 #E1E1E1;
+
+        }
+
+        $colors: ('black':$black, 'orange':$orange, 'blue': $blue, 'red':$red, 'green':$green);
+
+        @each $name, $color in $colors {
+          &.#{$name} {
+            background-color: $color;
+          }
+        }
+
+      }
+
+    }
+  }
+}
 
 </style>
