1a26dee4c2a737341c0afbd05c7cae7837e11a42
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-22 18:34:15 +0200

    style: styling range

--- components/SVG/reusable/FontSizeAdjuster.vue
+++ components/SVG/reusable/FontSizeAdjuster.vue
@@ -20,6 +20,103 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.fontsize-adjuster{
+  &--wrapper{
 
+    input{
+      // RANGE
+      -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
+      width: 100%; /* Specific width is required for Firefox. */
+      background: transparent; /* Otherwise white in Chrome */
+
+      &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+      }
+
+      &:focus {
+        outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+      }
+
+      &::-ms-track {
+        width: 100%;
+        cursor: pointer;
+
+        /* Hides the slider so custom styles can be added */
+        background: transparent;
+        border-color: transparent;
+        color: transparent;
+      }
+
+      // THUMB
+
+      @mixin thumb{
+        height: 17px;
+        width: 17px;
+        border-radius: 50%;
+        background: $clr-primary;
+        cursor: pointer;
+      }
+
+      /* Special styling for WebKit/Blink */
+      &::-webkit-slider-thumb {
+        @include thumb;
+        -webkit-appearance: none;
+        margin-top: -8px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+      }
+
+      /* All the same stuff for Firefox */
+      &::-moz-range-thumb {
+        @include thumb;
+      }
+
+      /* All the same stuff for IE */
+      &::-ms-thumb {
+        @include thumb;
+      }
+
+      //TRACK
+
+      @mixin tracker {
+        width: 100%;
+        height: 2px;
+        cursor: pointer;
+        background: $bg-tertiary;
+      }
+
+      &::-webkit-slider-runnable-track {
+        @include tracker;
+      }
+
+      &:focus::-webkit-slider-runnable-track {
+        @include tracker;
+      }
+
+      &::-moz-range-track {
+        @include tracker;
+      }
+
+      &::-ms-track {
+        width: 100%;
+        height: 2px;
+        cursor: pointer;
+        background: transparent;
+        color: transparent;
+      }
+      &::-ms-fill-lower {
+        background-color: $bg-tertiary;
+      }
+      &:focus::-ms-fill-lower {
+        background-color: $bg-tertiary;
+      }
+      &::-ms-fill-upper {
+        background-color: $bg-tertiary;
+      }
+      &:focus::-ms-fill-upper {
+        background-color: $bg-tertiary;
+      }
+
+    }
+  }
+}
 </style>
