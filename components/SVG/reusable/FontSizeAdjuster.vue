52f6d5bacb9d3831be1209b45471af5d467d59b4
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-22 18:38:41 +0200

    style: aligning items

--- components/SVG/reusable/FontSizeAdjuster.vue
+++ components/SVG/reusable/FontSizeAdjuster.vue
@@ -2,7 +2,9 @@
 <div class="fontsize-adjuster">
   <div class="fontsize-adjuster--wrapper">
     <label for="font-adjuster">Size: </label>
-    <input type="range" id="font-adjuster">
+    <div class="input">
+      <input type="range" id="font-adjuster">
+    </div>
     <p>{{currentFont}} px</p>
   </div>
 </div>
@@ -24,102 +26,111 @@ export default {
 .fontsize-adjuster{
   &--wrapper{
 
+    @apply flex items-center justify-between;
+
     label{
       font-weight: 500;
     }
 
-    input{
-      // RANGE
-      -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
-      width: 100%; /* Specific width is required for Firefox. */
-      background: transparent; /* Otherwise white in Chrome */
+    .input{
+      input{
 
-      &::-webkit-slider-thumb {
-        -webkit-appearance: none;
-      }
+        // RANGE
+        -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
+        width: 100%; /* Specific width is required for Firefox. */
+        background: transparent; /* Otherwise white in Chrome */
 
-      &:focus {
-        outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
-      }
+        &::-webkit-slider-thumb {
+          -webkit-appearance: none;
+        }
 
-      &::-ms-track {
-        width: 100%;
-        cursor: pointer;
+        &:focus {
+          outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+        }
 
-        /* Hides the slider so custom styles can be added */
-        background: transparent;
-        border-color: transparent;
-        color: transparent;
-      }
+        &::-ms-track {
+          width: 100%;
+          cursor: pointer;
 
-      // THUMB
+          /* Hides the slider so custom styles can be added */
+          background: transparent;
+          border-color: transparent;
+          color: transparent;
+        }
 
-      @mixin thumb{
-        height: 17px;
-        width: 17px;
-        border-radius: 50%;
-        background: $clr-primary;
-        cursor: pointer;
-      }
+        // THUMB
 
-      /* Special styling for WebKit/Blink */
-      &::-webkit-slider-thumb {
-        @include thumb;
-        -webkit-appearance: none;
-        margin-top: -8px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
-      }
+        @mixin thumb{
+          height: 17px;
+          width: 17px;
+          border-radius: 50%;
+          background: $clr-primary;
+          cursor: pointer;
+        }
 
-      /* All the same stuff for Firefox */
-      &::-moz-range-thumb {
-        @include thumb;
-      }
+        /* Special styling for WebKit/Blink */
+        &::-webkit-slider-thumb {
+          @include thumb;
+          -webkit-appearance: none;
+          margin-top: -8px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+        }
 
-      /* All the same stuff for IE */
-      &::-ms-thumb {
-        @include thumb;
-      }
+        /* All the same stuff for Firefox */
+        &::-moz-range-thumb {
+          @include thumb;
+        }
 
-      //TRACK
+        /* All the same stuff for IE */
+        &::-ms-thumb {
+          @include thumb;
+        }
 
-      @mixin tracker {
-        width: 100%;
-        height: 2px;
-        cursor: pointer;
-        background: $bg-tertiary;
-      }
+        //TRACK
 
-      &::-webkit-slider-runnable-track {
-        @include tracker;
-      }
+        @mixin tracker {
+          width: 100%;
+          height: 2px;
+          cursor: pointer;
+          background: $bg-tertiary;
+        }
 
-      &:focus::-webkit-slider-runnable-track {
-        @include tracker;
-      }
+        &::-webkit-slider-runnable-track {
+          @include tracker;
+        }
 
-      &::-moz-range-track {
-        @include tracker;
-      }
+        &:focus::-webkit-slider-runnable-track {
+          @include tracker;
+        }
 
-      &::-ms-track {
-        width: 100%;
-        height: 2px;
-        cursor: pointer;
-        background: transparent;
-        color: transparent;
-      }
-      &::-ms-fill-lower {
-        background-color: $bg-tertiary;
-      }
-      &:focus::-ms-fill-lower {
-        background-color: $bg-tertiary;
-      }
-      &::-ms-fill-upper {
-        background-color: $bg-tertiary;
-      }
-      &:focus::-ms-fill-upper {
-        background-color: $bg-tertiary;
-      }
+        &::-moz-range-track {
+          @include tracker;
+        }
 
+        &::-ms-track {
+          width: 100%;
+          height: 2px;
+          cursor: pointer;
+          background: transparent;
+          color: transparent;
+        }
+        &::-ms-fill-lower {
+          background-color: $bg-tertiary;
+        }
+        &:focus::-ms-fill-lower {
+          background-color: $bg-tertiary;
+        }
+        &::-ms-fill-upper {
+          background-color: $bg-tertiary;
+        }
+        &:focus::-ms-fill-upper {
+          background-color: $bg-tertiary;
+        }
+
+      }
+    }
+
+    p{
+      width: fit-content;
     }
   }
 }
