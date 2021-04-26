c25938d731da61745aba9dfb3307ebf4ea489884
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-26 09:37:13 +0200

    test : IconClassCopy.spec.js

--- test/components/IconEditor/IconClassCopy.spec.js
+++ test/components/IconEditor/IconClassCopy.spec.js
@@ -2,10 +2,8 @@
 import { mount} from '@vue/test-utils'
 import IconClassCopy from "~/components/IconEditor/IconClassCopy";
 import {describe, test} from "@jest/globals";
-const clipboardy = require('clipboardy');
 
 const iconClass = "si-arrow-bottom-left-o"
-const expectedCopyText = "<i class=\"si-arrow-bottom-left-o\"></i>"
 
 
 
@@ -17,9 +15,6 @@ const wrapper = mount(IconClassCopy, {
 
 
 describe('Mounted IconClassCopy',  ()=> {
-
-  const copyEl = wrapper.find('.copy-icon')
-
 
   test('test props', ()=> {
     expect(wrapper.props().iconClass).toBe(iconClass)
