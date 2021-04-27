ed7250b254fdd3033778b8d1df5e595b9e8530fe
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-27 22:35:34 +0200

    feat: icon collection html structure

---
+++ components/IconCollection/iconCollection.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="icon-collection">
+  <div class="icon-collection--wrapper">
+    <div class="head">
+      <h4>Collection <span class="number">4</span></h4>
+    </div>
+    <div class="icons">
+
+    </div>
+    <div class="customize">
+      <h4>customize</h4>
+      <div class="customize-font">
+
+      </div>
+      <div class="customize-color">
+
+      </div>
+    </div>
+    <div class="download">
+      <div class="download--svg"></div>
+      <div class="download--png"></div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "iconCollection"
+}
+</script>
+
+<style scoped>
+
+</style>
