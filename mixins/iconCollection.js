16201de7f3c58787086d896eace2621c09a5aca6
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-28 14:35:04 +0200

    feat: icon collection Mixin

---
+++ mixins/iconCollection.js
@@ -0,0 +1,12 @@
+const mixin = {
+  computed:{
+    storedIcons(){
+      if(process.browser){
+        const storedIcons = localStorage.getItem('storedIcons')
+        return storedIcons ? JSON.parse(storedIcons) : []
+      }
+      return []
+    }
+  }
+}
+ export default mixin
