292730061b6106f3f30aa3bd2e679f4949d8778f
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-04-29 16:02:21 +0200

    feat: creating event bus

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/highlight.js'
+    '~/plugins/highlight.js',
+    '~/plugins/eventBus.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
