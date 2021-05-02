64e6be6707917cd32def1bfd21c521650d7f054a
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-02 17:42:03 +0200

    new icons

--- components/IconPack/Icon.vue
+++ components/IconPack/Icon.vue
@@ -1,12 +1,22 @@
 <template>
-<div class="icon-card" :class="{hoverEffect:hoverEffect}" >
+<div class="icon-card" @mouseenter="mouseOver = true" @mouseleave="mouseOver = false" :class="{hoverEffect:hoverEffect,stored:stored}" >
   <div class="add-to-collection" @click="$emit('add')">
-    <div class="icon">
-      <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div class="add-icon icon" v-if="mouseOver && !stored">
+      <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M9 3.75V14.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M3.75 9H14.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     </div>
+    <div class="remove-icon icon" v-if="mouseOver && stored">
+      <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 3.75V14.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M3.75 9H14.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    </div>
+    <div class="stored-icon icon" v-if="!mouseOver && stored">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"/></svg>
+    </div>
+
   </div>
   <div class="icon-card--wrapper" @click="$emit('click')">
     <div class="icon">
@@ -23,7 +33,13 @@
 export default {
   name: "Icon",
   props:{
-    hoverEffect:{default:true}
+    hoverEffect:{default:true},
+    stored:{default:false},
+  },
+  data(){
+    return{
+      mouseOver:false,
+    }
   }
 
 }
@@ -37,7 +53,9 @@ export default {
   height:6rem;
   position: relative;
 
-  &.hoverEffect:hover{
+
+  &.hoverEffect:hover,
+  &.stored{
     .add-to-collection{
       display: block;
     }
@@ -58,6 +76,14 @@ export default {
 
     .icon{
       padding: 4px;
+
+      svg{
+        fill:$bg;
+      }
+
+      &.remove-icon{
+        transform: rotate(45deg);
+      }
 
     }
 
