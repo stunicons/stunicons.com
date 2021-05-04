88fef53fcc36685b1b045fd8695c0de8eac45e0e
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-04 10:21:28 +0200

    refactor: show in icon collection

--- components/IconPack/Icon.vue
+++ components/IconPack/Icon.vue
@@ -13,7 +13,7 @@
         <path d="M3.75 9H14.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     </div>
-    <div class="stored-icon icon" v-if="!mouseOver && stored">
+    <div class="stored-icon icon" v-if="!mouseOver && stored && hoverEffect">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"/></svg>
     </div>
 
