d8e08c8f6669e4c4119578b1a8033722dc34237f
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-07 01:53:55 +0200

    feat: improved way of requesting icon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,7 +10,7 @@
       </div>
       <div class="navbar--action-btn">
         <div class="action-btn--request-icon">
-          <a href="https://github.com/Stunnity/stunicons/blob/main/RequestingIcon.md" target="_blank">Request Icon</a>
+          <a href="https://github.com/Stunnity/stunicons/issues/new?assignees=Daniel-Dukundane%2C+Gustavy&labels=enhancement%2C+request+icon&template=request-icon.md&title=Requesting+Icon" target="_blank">Request Icon</a>
         </div>
         <div class="action-btn--go-to-github">
           <a target="_blank" href="https://github.com/Stunnity/stunicons">
