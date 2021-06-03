2522918e9ad32d834058fcc70a0e756e79d7b4af
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-03 09:53:35 +0200

    configuring g-tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/highlight.js',
-    '~/plugins/eventBus.js'
+    '~/plugins/eventBus.js',
+    { src: '~/plugins/ga.client.js', mode:'client'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
