98c051337061f99dcdd6ffc93b1c9c5887d37170
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-06-14 23:17:20 +0200

    perf: node compatability

--- utils/svgToElement.js
+++ utils/svgToElement.js
@@ -1,5 +1,11 @@
 
 export default function dataUriToSvg(dataUri){
-  console.log(dataUri)
-  return atob(dataUri.replace(/data:image\/svg\+xml;base64,/, ''));
+  const g  = window || global;
+  const b64  = dataUri.replace(/data:image\/svg\+xml;base64,/, '');
+
+  if (g.atob) {
+      return  g.atob(b64);
+  } else {
+      return  Buffer.from(b64, 'base64').toString('utf8');
+  }
 }
